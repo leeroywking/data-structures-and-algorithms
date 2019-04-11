4a9979fe481a35c8c9fa57025f466d20c626f9cb
Ubuntu <ubuntu@ip-10-0-8-35.us-west-2.compute.internal>
2019-04-11 18:02:00 +0000

    finished 3

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -45,8 +45,11 @@ Write a function named sortByLength that takes in an array of strings and return
 ------------------------------------------------------------------------------------------------ */
 
 const sortByLength = (arr) => {
-  // Solution code here...
-};
+arr.sort( (a,b) => { 
+  return a.length-b.length; 
+});
+  return arr;
+}
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
