635fdb5336e05e77a756818034524f300d24650e
Ubuntu <ubuntu@ip-10-0-8-35.us-west-2.compute.internal>
2019-04-23 21:02:22 +0000

    solved 1

--- code-challenges/7-map/challenges-07.test.js
+++ code-challenges/7-map/challenges-07.test.js
@@ -10,9 +10,7 @@ You may choose to complete this challenge using a for loop, for...in syntax, or 
 For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
 ------------------------------------------------------------------------------------------------ */
 
-const forLoopTwoToThe = (arr) => {
-  // Solution code here...
-};
+const forLoopTwoToThe = (arr) => arr.map( power  => Math.pow(2,power));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
