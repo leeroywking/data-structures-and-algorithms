e86858c7b36a0fc86b65ecd5c119da5ddcdf5ffa
leeroywking <leeroywking@gmail.com>
2019-08-01 20:10:42 -0700

    100% coverage

--- challenges/multiBracketValidation/multiBracketValidation.test.js
+++ challenges/multiBracketValidation/multiBracketValidation.test.js
@@ -7,4 +7,7 @@ describe('multi bracket validation test', () => {
   it('validates a simple bracket string ', () => {
     expect(multiValidate('(a useless series of letters {} [])')).toBeTruthy();
   });
+  it('invalidates a bad bracket string', () => {
+    expect(multiValidate('{{{{{ this is a bad string')).toBeFalsy();
+  });
 });
