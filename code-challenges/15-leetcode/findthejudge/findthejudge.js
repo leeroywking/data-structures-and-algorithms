82895a4dcdb65dfee355d1b2c7c9fed668da746f
leeroywking <leeroywking@gmail.com>
2019-10-10 23:36:48 -0700

    find the judge very bad solution

---
+++ code-challenges/15-leetcode/findthejudge/findthejudge.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/**
+ * @param {number} N
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(N, trust) {
+    let possJudges = new Set()
+    let map = new Map();
+    for(let i = 1; i <=N ; i++){
+      map.set(i, [])
+      possJudges.add(i)
+    }
+    trust.forEach(pair => {
+      map.get(pair[0]).push(pair[1])
+      possJudges.delete(pair[0])
+    })
+    let maybejudge
+    if(possJudges.size === 1){
+      maybejudge = Array.from(possJudges)[0]
+    }
+    for(let i = 1; i<= N ; i++){
+      if(i !== maybejudge){
+        if(!map.get(i).includes(maybejudge)){
+          return -1
+        }
+      }
+    }
+    return maybejudge ? maybejudge : -1
+  }
+
+  module.exports = findJudge;
---
+++ code-challenges/15-leetcode/poweroffour/poweroffour.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ * 
+ */
+const isPowerOfFourOld = function (num) {
+    let set = new Set()
+    if (num === 1) { return true }
+    let checker = 1
+    while (checker < 2147483647 && checker <= num) {
+        set.add(checker)
+        if (num === checker) { return true  }
+        else (checker = checker * 4)
+    }
+    return false
+};
+
+function isPowerOfFour(num) {
+    let base = 4
+    let power = 0;
+    let set  = new Set();
+    while (power < 15) {
+        let current = Math.pow(base, power);
+        set.add(current)
+        power = power + 1
+    }
+    console.log(Array.from(set))
+    return set.has(num)
+}
+
+module.exports = isPowerOfFour
+
+// isPowerOfFour(4)
+isPowerOfFourOld(1073741824)
