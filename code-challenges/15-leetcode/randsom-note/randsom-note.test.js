8331ac9d7554bcb076b96decdf4f7b92c03ba619
leeroywking <leeroywking@gmail.com>
2019-10-11 21:47:08 -0700

    randsom-note working

---
+++ code-challenges/15-leetcode/randsom-note/randsom-note.test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let canConstruct = require('./randsom-note.js')
+
+describe('Randsome note tests', () => {
+    it('passes the example test 1', () => {
+        expect(canConstruct('a','b')).toBeFalsy()
+    })
+    it('passes the leetcode examples 2', () => {
+        expect(canConstruct('aa','ab')).toBeFalsy()
+    })
+    it('it passes the leetcode examples 3', () => {
+        expect(canConstruct('aa','aab')).toBeTruthy()
+    })
+})
