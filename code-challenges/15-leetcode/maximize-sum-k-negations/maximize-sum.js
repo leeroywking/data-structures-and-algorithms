f197cb92f09822227779be71e4e02566f8a31a69
leeroywking <leeroywking@gmail.com>
2019-10-23 18:21:32 -0700

    passed maximize-sum with 100% space

--- code-challenges/15-leetcode/maximize-sum-k-negations/maximize-sum.js
+++ code-challenges/15-leetcode/maximize-sum-k-negations/maximize-sum.js
@@ -1,14 +1,20 @@
+'use strict'
 /**
  * @param {number[]} A
  * @param {number} K
  * @return {number}
  */
-var largestSumAfterKNegations = function(A, K) {
-    let smallestIdx
-    for(let i = 0; i < A.length ;  i++){
-        if(smallest === undefined){smallest = A[i]}
+var largestSumAfterKNegations = function (A, K) {
+  for (let j = 0; j < K; j++) {
+    let smallestIdx = 0
+    for (let i = 0; i < A.length; i++) {
+      if (A[smallestIdx] > A[i]) { 
+        smallestIdx = i 
+      }
     }
-    
+    A[smallestIdx] = A[smallestIdx] * -1
+  }
+  return A.reduce((a, b) => a + b)
 };
 
 module.exports = largestSumAfterKNegations
